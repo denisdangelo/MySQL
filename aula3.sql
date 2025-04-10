35b3bed056e708bce96a79a7126ec7fee6f5a6e8
DenisDS <denis.dsilveira1@outloog.com>
2025-04-09 22:21:43 -0300

    Estudos sobre o comando SELECT

---
+++ aula3.sql
@@ -0,0 +1,25 @@
+/* cadastrar clientes na tabela alunos */
+
+INSERT INTO alunos VALUE
+
+(default, 'Sirlene', 'Professor', '1979-08-10', 'F', '60.00', '1.56', 'Brasil’),
+(default, 'José de Assis', 'Professor', '1971-04-10', 'M', '60', '1.60', default),
+(default, 'Leandro', 'Professor', '1979-04-02', 'M', '98.5', '1.75', default),
+(default, 'Robson', 'Professor', '1979-07-02', 'M', '65', '1.70', default),
+(default, 'Andres', 'Dentista', '1970-01-01', 'M', '70', '1.70', 'Irlanda’),
+(default, 'Edinaldo', 'Empreendedor', '1970-10-10', 'M', '74.65', '1.85', 'EUA’),
+(default, 'Érica', 'Esteticista', '2013-12-30', 'F', '58.5', '1.68', default),
+(default, 'Erick', 'Auxiliar Adm', '1993-04-22', 'M', '98.2', '1.95', default),
+(default, 'Gabriela', 'Atora', '1994-03-12', 'F', '52.9', '1.62', 'Canadá’),
+(default, 'Gabriel', 'Professor', '1989-12-01', 'M', '64.22', '1.65', 'Portugal’),
+(default, 'Gustavo Pinheiro', 'Empreendedor', '2000-06-23', 'M', '77.44', '1.66', 'EUA’),
+(default, 'Gustavo Teodoro', 'Programador', '1995-03-12', 'M', '88.5', '1.76', 'Inglaterra’),
+(default, 'Julia', 'Dentista', '1988-01-01', 'F', '55.2', '1.65', 'França’),
+(default, 'Luiz', 'Contador', '2011-01-01', 'M', '54.11', '1.58', default),
+(default, 'Milena', 'Dentista', '2006-08-11', 'F', '88.7', '1.78', default),
+(default, 'Paulo', 'Contador', '1993-07-01', 'M', '70.3', '1.65', default),
+(default, 'Ryan', 'Arquiteto', '1968-10-11', 'M', '88.88', '1.64', 'Argentina’),
+(default, 'Vinicius', 'Engenheiro Eletrico', '1984-11-01', 'M', '75.12', '1.63', 'Argentina’),
+(default, 'Vitor', 'Engenheiro Civil', '1970-01-01', 'M', '70', '1.70', 'Uruguaí’),
+(default, 'Wesley Carlos', 'Mecanico', '2000-03-03', 'M', '82', '1.78', 'Portugal’),
+(default, 'Wesley Moreira', 'Jogador de Futebol', '1996-05-17', 'M', '65.8', '1.682', default);
