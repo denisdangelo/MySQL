514b4b8da5b1b9621f334addc8fea6d673e0e2d1
DenisDS <denis.dsilveira1@outloog.com>
2025-04-28 22:34:14 -0300

    introdução ao diagrama E-R DER

---
+++ aula05.sql
@@ -0,0 +1,10 @@
+/*DIAGRAMA E-R - DER
+
+Tabela = entidade e é representada por um retangulo
+Tupla = valor dos atributos (caracteristicas (nome, id,); Celula; 
+Relação = Losango 
+CLASSIFICAÇÂO PARA RELACIONAMENTOS:
+cardinalidade = n para n (muitos para muitos); 1 para 1; 1 para n (um para muitos)
+
+*/
+
